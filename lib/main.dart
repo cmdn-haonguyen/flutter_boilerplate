962f8f162748ebac31a799a5ba117ddd4ace87c5
cmdn-haonguyen <hao.nguyen@classmethod.vn>
2025-06-04 09:30:41 +0700

    feat: update Home page

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,37 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_boilerplate/core/Router/app_router.dart';
+import 'package:flutter_boilerplate/data/repositories/movie_repository.dart';
+import 'package:flutter_boilerplate/presentation/bloc/movie_bloc.dart';
+import 'package:flutter_boilerplate/presentation/bloc/movie_event.dart';
+import 'core/network/api_service.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() {
-  runApp(const MyApp());
+  final apiService = ApiService();
+  final movieRepository = MovieRepository(apiService);
+
+  runApp(MyApp(movieRepository: movieRepository));
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  final MovieRepository movieRepository;
+
+  const MyApp({
+    super.key,
+    required this.movieRepository,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
-      debugShowCheckedModeBanner: false,
-      routerConfig: AppRouter.router,
+    return RepositoryProvider.value(
+      value: movieRepository,
+      child: BlocProvider(
+        create: (_) => MovieBloc(movieRepository)..add(FetchMovies()),
+        child: MaterialApp.router(
+          debugShowCheckedModeBanner: false,
+          routerConfig: AppRouter.router,
+        ),
+      ),
     );
   }
 }
--- lib/presentation/pages/home_page.dart
+++ lib/presentation/pages/home_page.dart
@@ -1,10 +1,49 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_boilerplate/core/network/api_service.dart';
+import 'package:flutter_boilerplate/data/repositories/movie_repository.dart';
+import 'package:flutter_boilerplate/models/movie.dart';
+import 'package:flutter_boilerplate/presentation/bloc/movie_event.dart';
+import 'package:flutter_boilerplate/presentation/bloc/movie_state.dart';
+import 'package:flutter_boilerplate/presentation/bloc/movie_bloc.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return BlocProvider(
+      create: (context) =>
+      MovieBloc(MovieRepository(ApiService()))..add(FetchMovies()),
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('Home'),
+        ),
+        body: BlocBuilder<MovieBloc, MovieState>(
+          builder: (context, state) {
+            if (state is MovieLoading) {
+              return Center(child: CircularProgressIndicator());
+            } else if (state is MovieLoaded) {
+              return ListView.builder(
+                itemCount: state.movies.length,
+                itemBuilder: (context, index) {
+                  final Movie movie = state.movies[index];
+                  return ListTile(
+                    leading: Image.network(
+                      'https://image.tmdb.org/t/p/w92${movie.posterPath}',
+                      width: 50,
+                    ),
+                    title: Text(movie.title),
+                  );
+                },
+              );
+            } else if (state is MovieError) {
+              return Center(child: Text(state.message));
+            }
+            return Container();
+          },
+        ),
+      ),
+    );
   }
 }
