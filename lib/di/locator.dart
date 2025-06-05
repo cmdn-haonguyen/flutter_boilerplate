e78ef77a61650383905985a09d0cb0aeae30dd91
cmdn-haonguyen <hao.nguyen@classmethod.vn>
2025-06-05 11:11:46 +0700

    feat: update locator inject repository, usecase

--- lib/di/locator.dart
+++ lib/di/locator.dart
@@ -1,10 +1,22 @@
+import 'package:flutter_boilerplate/domain/usecase/get_now_playing_movies.dart';
 import 'package:get_it/get_it.dart';
 import 'package:flutter_boilerplate/core/network/api_service.dart';
-import 'package:flutter_boilerplate/data/repositories/movie_repository.dart';
+import 'package:flutter_boilerplate/domain/repository/movie_repository.dart';
+import 'package:flutter_boilerplate/data/repositories/movie_repository_Impl.dart';
 
 final getIt = GetIt.instance;
 
 void setupLocator() {
+  // Core
   getIt.registerLazySingleton(() => ApiService());
-  getIt.registerLazySingleton(() => MovieRepository(ApiService()));
+
+  // Repositories
+  getIt.registerLazySingleton<MovieRepositoryImpl>(
+    () => MovieRepositoryImpl(getIt<ApiService>()),
+  );
+
+  // Use Case
+  getIt.registerLazySingleton<GetNowPlayingMovies>(
+      () => GetNowPlayingMovies(getIt<MovieRepositoryImpl>())
+  );
 }
